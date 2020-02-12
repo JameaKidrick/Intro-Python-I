6dba979d3b3831ffa8a8bdc4462bec51a2e97beb
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-02-12 04:14:09 -0500

    finished assignment 14, but it needs to be optimized

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,17 +6,17 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
- - If the user doesn't specify any input, your program should
-   print the calendar for the current month. The 'datetime'
-   module may be helpful for this.
- - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
-   both the month and the year. Render the calendar for that
-   month and year.
- - Otherwise, print a usage statement to the terminal indicating
-   the format that your program expects arguments to be given.
-   Then exit the program.
+  - If the user doesn't specify any input, your program should
+    print the calendar for the current month. The 'datetime'
+    module may be helpful for this.
+  - If the user specifies one argument, assume they passed in a
+    month and render the calendar for that month of the current year.
+  - If the user specifies two arguments, assume they passed in
+    both the month and the year. Render the calendar for that
+    month and year.
+  - Otherwise, print a usage statement to the terminal indicating
+    the format that your program expects arguments to be given.
+    Then exit the program.
 
 Note: the user should provide argument input (in the initial call to run the file) and not 
 prompted input. Also, the brackets around year are to denote that the argument is
@@ -30,3 +30,23 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+date = sys.argv 
+today = datetime.date(datetime.today())
+def cal(year = today.year, month = today.month):
+    if len(date) == 2 and int(date[1]) > 12:
+        month = int(date[1])
+        print(month)
+        return calendar.TextCalendar().formatmonth(year, month)
+    elif len(date) == 1:
+        return calendar.TextCalendar().formatmonth(year, month)
+    elif len(date) == 3:
+        year = int(date[2])
+        month = int(date[1])
+        return calendar.TextCalendar().formatmonth(year, month)
+    else:
+        print('Please give your input in the format: `14_cal.py [month] [year]`')
+print('CALENDAR', cal())
+
+
+
